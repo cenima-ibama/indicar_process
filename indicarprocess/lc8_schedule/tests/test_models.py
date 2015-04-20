2c32d2076e9acc68f393376c1e5876e3033cc143
Wille Marcel <wille@wille.blog.br>
2015-04-20 14:48:41 -0300

    improve tests

--- indicarprocess/lc8_schedule/tests/test_models.py
+++ indicarprocess/lc8_schedule/tests/test_models.py
@@ -8,15 +8,16 @@ from ..models import ScheduledScene
 
 class TestScheduledScene(TestCase):
 
-    def test_scheduled_scene_creation(self):
-
-        scene = ScheduledScene.objects.create(path='001', row='001',
+    def setUp(self):
+        self.scene = ScheduledScene.objects.create(path='001', row='001',
             last_date=(date.today() - timedelta(days=17)))
-        self.assertEqual(scene.__str__(), 'LC8 001-001')
+
+    def test_scheduled_scene_creation(self):
+        self.assertEqual(self.scene.__str__(), 'LC8 001-001')
 
         ScheduledScene.objects.create(path='001', row='002')
+        self.assertEqual(ScheduledScene.objects.all().count(), 2)
 
+    def test_validation(self):
         with self.assertRaises(ValidationError):
             ScheduledScene.objects.create(path='001', row='001')
-
-        self.assertEqual(ScheduledScene.objects.all().count(), 2)
