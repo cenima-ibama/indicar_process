94d251b3f0e2a1c420aa2ff6f6a7f59ed96235c4
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-05-26 18:32:41 -0300

    improve add_image and make_tms functions to avoid errors

--- indicarprocess/catalogo/tasks.py
+++ indicarprocess/catalogo/tasks.py
@@ -7,20 +7,29 @@ from .models import CatalogoLandsat
 
 def add_image(image):
     """Function to add a Image to CatalogoLandsat."""
-    CatalogoLandsat.objects.get_or_create(
-        image=image.name,
-        path=join('/mnt/csr/imagens/landsat8', image.scene.name),
-        shape=image.scene.geom,
-        data=image.scene.date,
-        url_tms=join('http://10.1.25.66/imagens/tms/landsat',
-            image.scene.name + '_r6g5b4_tms.xml')
-    )
+    if image.type == 'r6g5b4':
+        if CatalogoLandsat.objects.filter(image=image.name).count() == 0:
+            CatalogoLandsat.objects.create(
+                image=image.name,
+                path=join('/mnt/csr/imagens/landsat8', image.scene.name),
+                shape=image.scene.geom,
+                data=image.scene.date,
+                url_tms=join('http://10.1.25.66/imagens/tms/landsat',
+                    image.scene.name + '_r6g5b4_tms.xml')
+            )
+        else:
+            print(('Catalogo Landsat to %s already exists.' % image.name))
+    else:
+        print('Image is not of r6g5b4 type.')
 
 
 def make_tms(image):
     """Generate the TMS of an Image."""
     if image.type == 'r6g5b4':
-        path = join('/csr/imagens/landsat8', image.file_path())
-        call(['make_tms.sh', path])
+        if CatalogoLandsat.objects.filter(image=image.name).count() == 0:
+            path = join('/csr/imagens/landsat8', image.file_path())
+            call(['make_tms.sh', path])
+        else:
+            print(('%s already has TMS' % image.name))
     else:
         print('Image is not of r6g5b4 type.')
