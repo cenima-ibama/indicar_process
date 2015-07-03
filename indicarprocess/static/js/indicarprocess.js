21eae4271cd43fdd7f5a3cb6f4e00c7db8d000cc
Wille Marcel <wille.malheiro@ibama.gov.br>
2015-07-03 12:49:42 -0300

    fix small error on satellite select form

--- indicarprocess/static/js/indicarprocess.js
+++ indicarprocess/static/js/indicarprocess.js
@@ -1,51 +1,42 @@
 /* Project specific Javascript goes here. */
 $(document).ready(function(){
+  $("#startDate").on("change",function(){
+    $("#endDate").datepicker('setStartDate',$(this).val());
+  });
 
-	$("#startDate").on("change",function(){
-		$("#endDate").datepicker('setStartDate',$(this).val());
-	});
+  $("#cloudRange").slider();
 
+  $("#search").on("click", function(){
+    var url = "//" + window.location.host + "/?";
 
-	$("#cloudRange").slider();
+    var scene = $("#scene").val();
+    var path = $("#path").val();
+    var row = $("#row").val();
+    var sat = $("#sat").val();
+    var status = $("#status").val();
+    var maxcloud = $("#cloudRange").val();
+    var start = $("#startDate").val();
+    var end = $("#endDate").val();
 
-	$("#search").on("click", function(){
+    if (scene && scene != "")
+      url = url + "name=" + scene + "&";
+    if (path && path != "")
+      url = url + "path=" + path + "&";
+    if (row && row != "")
+      url = url + "row=" + row + "&";
+    if (sat && sat != "")
+      url = url + "sat=" + sat + "&";
+    if (status && status != "")
+      url = url + "status=" + status + "&";
+    if ( maxcloud && maxcloud != "")
+      url = url + "max_cloud=" + maxcloud + "&";
+    if (start && start != "")
+      url = url + "start=" + start + "&";
+    if (end && end != "")
+      url = url + "end=" + end + "&";
 
-		var url = "//" + window.location.host + "/?";
-
-		var scene = $("#scene").val();
-		var path = $("#path").val();
-		var row = $("#row").val();
-		var satellite = $("#satellite").val();
-		var status = $("#status").val();
-		var maxcloud = $("#cloudRange").val();
-		var start = $("#startDate").val();
-		var end = $("#endDate").val();
-
-		if (scene && scene != "")
-			url = url + "name=" + scene + "&";
-		if (path && path != "")
-			url = url + "path=" + path + "&";
-		if (row && row != "")
-			url = url + "row=" + row + "&";
-		if (satellite && satellite != "")
-			url = url + "sat=" + satellite + "&";
-		if (status && status != "")
-			url = url + "status=" + status + "&";
-		if ( maxcloud && maxcloud != "")
-			url = url + "max_cloud=" + maxcloud + "&";
-		if (start && start != "")
-			url = url + "start=" + start + "&";
-		if (end && end != "")
-			url = url + "end=" + end + "&";
-
-
-		url = url.substring(0,url.length - 1);
-
-
-		this.href = url;
-
-		$(this).click();
-
-	});
-
+    url = url.substring(0,url.length - 1);
+    this.href = url;
+    $(this).click();
+  });
 });
