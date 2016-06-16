92ccc4fae85f8eca2e4960c7904cc9a9e47ee644
Lucas Lamounier Sirqueira <lucas.sirqueira@ibama.gov.br>
2016-06-16 16:49:53 -0300

    Add function of generate contrast gdal_contrast_stretch

--- indicarprocess/catalogo/bin/make_tms.sh
+++ indicarprocess/catalogo/bin/make_tms.sh
@@ -10,10 +10,10 @@ if [ ! -f "$rgbname_img" ] ; then
   exit 1
 fi
 # -- Creating TMS
-#fcontrast=$rgbname_img".contrast"
+fcontrast=$rgbname_img".contrast"
 16b_2_8b_convert.sh $rgbname_img > /dev/null
-#gdal_contrast_stretch -ndv 0 -outndv 0 -percentile-range 0.02 0.98  $rgbname_img $fcontrast  > /dev/null
-#mv -f $fcontrast $rgbname_img
+gdal_contrast_stretch -ndv 0 -outndv 0 -percentile-range 0.02 0.98  $rgbname_img $fcontrast  > /dev/null
+mv -f $fcontrast $rgbname_img
 mk_tiles.sh $rgbname_img 2 15 $output_png_path $output_tms_path $link_base
 # --
 code=$?
